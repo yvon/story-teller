207435e9d7293bb6dd3b38c973d4d94d0071ec87
Yvon Cognard <yvon@cognard.com>
2023-06-09 18:14:12 +0200

    Refactor narrator

--- src/narrator.rs
+++ src/narrator.rs
@@ -111,14 +111,16 @@ pub struct Story {
 impl Story {
     pub async fn new(service: Service) -> Self {
         let content = include_str!("initial_prompt.txt").to_string();
-        let current_chapter = Arc::new(Chapter::load(&service, None, content).await);
-        let next_chapters = next_chapters(&service, &current_chapter);
+        let chapter = Chapter::load(&service, None, content).await;
 
-        Self {
+        let mut story = Self {
             service,
-            current_chapter,
-            next_chapters,
-        }
+            current_chapter: Arc::new(chapter),
+            next_chapters: Vec::new(),
+        };
+
+        story.preload_next_chapters();
+        story
     }
 
     pub fn text(&self) -> &String {
@@ -136,21 +138,22 @@ impl Story {
     pub async fn choose(&mut self, index: usize) {
         let chapter = self.next_chapters.swap_remove(index).await.unwrap();
         self.current_chapter = Arc::new(chapter);
-        self.next_chapters = next_chapters(&self.service, &self.current_chapter);
+        self.preload_next_chapters();
     }
-}
 
-fn next_chapters(service: &Service, current_chapter: &Arc<Chapter>) -> Vec<JoinHandle<Chapter>> {
-    current_chapter
-        .as_ref()
-        .choices
-        .iter()
-        .map(|choice| {
-            let service = service.clone();
-            let content = choice.clone();
-            let parent = Some(current_chapter.clone());
+    fn preload_next_chapters(&mut self) {
+        self.next_chapters = self
+            .current_chapter
+            .as_ref()
+            .choices
+            .iter()
+            .map(|choice| {
+                let service = self.service.clone();
+                let content = choice.clone();
+                let parent = Some(self.current_chapter.clone());
 
-            tokio::task::spawn(async move { Chapter::load(&service, parent, content).await })
-        })
-        .collect()
+                tokio::task::spawn(async move { Chapter::load(&service, parent, content).await })
+            })
+            .collect()
+    }
 }
