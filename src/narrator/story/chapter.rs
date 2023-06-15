2b92c4e4660e28e96651820f5861b3b754257698
Yvon Cognard <yvon@cognard.com>
2023-06-15 09:59:48 +0200

    Implement retries

--- src/narrator/story/chapter.rs
+++ src/narrator/story/chapter.rs
@@ -3,11 +3,17 @@ use crate::narrator::linked_messages::{LinkedMessage, SharedMessage};
 use serde::{self, Deserialize};
 use std::sync::{Arc, RwLock};
 
+static MAX_ATTEMPTS: u32 = 3;
+
 pub struct Chapter {
     text: String,
     message: SharedMessage,
     total_tokens: u32,
     choices: Vec<String>,
+}
+
+struct Request {
+    message: LinkedMessage,
 }
 
 #[derive(Deserialize)]
@@ -18,20 +24,10 @@ struct ChatResponse {
 
 impl Chapter {
     pub async fn load(service: &Service, parent: Option<SharedMessage>, content: String) -> Self {
-        let query = LinkedMessage {
-            message: Message {
-                role: Role::User,
-                content: Some(content),
-                name: None,
-                function_call: None,
-            },
-            parent: parent.clone(),
-        };
-
-        let (response, total_tokens) = submit(&service, &query).await;
-        let parsed_response = parse_response(&response);
+        let request = Request::new(parent.clone(), content);
+        let (parsed_response, total_tokens) = request.perform(service).await;
         let text = parsed_response.text.clone();
-        let choices = parsed_response.choices;
+        let choices = choices(parsed_response.choices);
 
         // I've chosen to recreate the message because there is a bug with OpenAI API: it doesn't
         // accept messages without content attribute. It should also helps reducing the number of
@@ -73,6 +69,14 @@ impl Chapter {
     }
 }
 
+fn choices(as_returned_by_chat_gpt: Vec<String>) -> Vec<String> {
+    if as_returned_by_chat_gpt.len() < 1 {
+        vec![include_str!("default_choice.txt").to_string()]
+    } else {
+        as_returned_by_chat_gpt
+    }
+}
+
 fn parameters() -> serde_json::Value {
     serde_json::from_str(include_str!("parameters_schema.json")).unwrap()
 }
@@ -94,6 +98,46 @@ fn body(messages: Vec<Message>) -> request::Body {
     }
 }
 
+impl Request {
+    fn new(parent: Option<SharedMessage>, content: String) -> Self {
+        Self {
+            message: LinkedMessage {
+                message: Message {
+                    role: Role::User,
+                    content: Some(content),
+                    name: None,
+                    function_call: None,
+                },
+                parent,
+            },
+        }
+    }
+
+    async fn perform(&self, service: &Service) -> (ChatResponse, u32) {
+        let mut attempts = 0;
+
+        while attempts < MAX_ATTEMPTS {
+            match self.perform_once(service).await {
+                Ok(value) => return value,
+                Err(error) => eprintln!("Error: {}", error),
+            }
+
+            attempts += 1;
+        }
+
+        panic!("Max attempts reached");
+    }
+
+    async fn perform_once(&self, service: &Service) -> Result<(ChatResponse, u32), String> {
+        let (response, total_tokens) = submit(service, &self.message).await;
+
+        match parse_response(&response) {
+            Ok(value) => Ok((value, total_tokens)),
+            Err(error) => Err(error.to_string()),
+        }
+    }
+}
+
 async fn submit(service: &Service, linked_message: &LinkedMessage) -> (Message, u32) {
     let body = body(linked_message.messages());
     let api_response = service.submit(body).await;
@@ -104,12 +148,12 @@ async fn submit(service: &Service, linked_message: &LinkedMessage) -> (Message, 
     (response_message, total_tokens)
 }
 
-fn parse_response(message: &Message) -> ChatResponse {
+fn parse_response(message: &Message) -> serde_json::Result<ChatResponse> {
     let function_call = message.function_call.as_ref().expect("No function call");
 
     if function_call.name != "chapter" {
         panic!("Expected a chapter function call");
     }
 
-    serde_json::from_str(&function_call.arguments).unwrap()
+    serde_json::from_str(&function_call.arguments)
 }
